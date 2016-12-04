d31362aabc026f43d7186a053f0c541d863efa2b
cyberplanner <sal@salweb.co.uk>
2016-12-04 19:49:42 +0000

    Trainer knows when Kudomons are near.

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,16 +1,19 @@
 
 class Trainer
   attr_reader :location
-
+  attr_reader :near_by
   def initialize
     @location = [0, 0]
+    @near_by = []
   end
 
   def nearby_kudomons(array_of_kudomons)
     @all_kudomons = array_of_kudomons
-    @neary_by = []
     @all_kudomons.each do |kudomon|
-
+      location = kudomon.location
+      if location[0] < 25 && location[1] < 25
+        @near_by << kudomon
+      end
     end
   end
 end
--- spec/trainer_spec.rb
+++ spec/trainer_spec.rb
@@ -11,13 +11,15 @@ describe Trainer do
     expect(ash.location).to eq [0, 0]
   end
 
-  # context "there are kudomons in the area" do
-  #   let(:mancharred) { double("Mancharred", :class => FireType, :species => "Mancharred", :position => [50, 5] ) }
-  #   let(:sourbulb) { double("Sourbulb", :class => GrassType, :species => "Sourbulb", :position => [10, 10] ) }
-  #   it 'know which one is the nearest' do
-  #     expect(ash).to respond_to(:nearby_kudomons).with(1).argument
-  #     expect(ash.nearby_kudomons).to eq sourbulb
-  #   end
-  # end
+  context "there are kudomons in the area" do
+    let(:mancharred) { double("Mancharred", :class => FireType, :species => "Mancharred", :location => [50, 5] ) }
+    let(:sourbulb) { double("Sourbulb", :class => GrassType, :species => "Sourbulb", :location => [10, 10] ) }
+    let(:array_of_kudomons ) { [mancharred, sourbulb] }
+    it 'know which kudomons are near in the area' do
+      expect(ash).to respond_to(:nearby_kudomons).with(1).argument
+      ash.nearby_kudomons(array_of_kudomons)
+      expect(ash.near_by.first).to eq sourbulb
+    end
+  end
 
 end
